125ef067724763c83a83a0338aaac96dfe634bf0
Romil Kumar Vasani <romil.vasani@ticketmaster.com>
2016-10-23 19:02:33 -0700

    Current working version

---
+++ avg_per_learn.py
@@ -0,0 +1,110 @@
+import os, sys
+from pprint import pprint
+import math, random
+
+def get_features(token_list, file):
+    file_open = open(file, "r", encoding="latin1")
+    file_content = file_open.read()
+
+    file_content_tokenized = set(file_content.split())
+    token_list.update(file_content_tokenized)
+    return token_list
+
+def get_file_tokens(file):
+    file_open = open(file, "r", encoding="latin1")
+    file_content = file_open.read()
+
+    file_content_tokenized = file_content.split()
+
+    return file_content_tokenized
+
+def calculate_alpha(feature_list, tokens, bias):
+    val = 0
+    for token in tokens:
+        val += feature_list[token]
+    val += bias
+    return val
+
+def update_weight_for_tokens(feature_list, tokens, y):
+    for token in tokens:
+        feature_list[token] = feature_list[token] + y
+    return feature_list
+
+def update_weight_for_avg_tokens(feature_list, tokens, y, c):
+    for token in tokens:
+        feature_list[token] = feature_list[token] + y*c
+    return feature_list
+
+def write_to_file(bias, feature_list):
+    out = {}
+    out["bias"] = bias
+    out["model"] = feature_list
+    target = open('per_model.txt', 'a')
+    target.write(str(out))
+
+def main_func():
+    directory = sys.argv
+
+    max_iterations = 30
+    token_list = set()
+    file_in_memory = {}
+
+    for root, dirs, files in os.walk(directory[1]):
+        for file in files:
+            if file.endswith(".txt"):
+                token_list = get_features(token_list, os.path.join(root, file))
+                file_in_memory[os.path.join(root, file)] = get_file_tokens(os.path.join(root, file))
+
+    feature_list = {}
+    avg_feature_list = {}
+
+    for item in token_list:
+        feature_list[item] = 0
+        avg_feature_list[item] = 0
+
+    y_spam = 1
+    y_ham = -1
+
+    bias = 0
+    avg_bias = 0
+
+    c = 1
+
+    num_iterations = 0
+    num_features = len(feature_list)
+
+    for i in range(max_iterations):
+        for root, dirs, files in os.walk(directory[1]):
+            random.shuffle(files)
+            for file in files:
+                y = 0
+                if file.endswith(".txt"):
+                    if "spam" in file:
+                        y = y_spam
+                    else:
+                        y = y_ham
+
+                    tokens = file_in_memory[os.path.join(root, file)]
+                    alpha = calculate_alpha(feature_list, tokens, bias)
+                    if (alpha * y) <= 0:
+                        feature_list = update_weight_for_tokens(feature_list, tokens, y)
+                        avg_feature_list = update_weight_for_avg_tokens(avg_feature_list, tokens, y, c)
+                        bias += y
+                        avg_bias += y*c
+                    c = c + 1
+                    # print(alpha, bias)
+                    # for token in tokens:
+                    #     print(token, feature_list[token]),
+                    # print()
+                # input()
+    # pprint(feature_list)
+
+    cal_support = 1/c
+    for item in token_list:
+        avg_feature_list[item] = feature_list[item] - (cal_support * avg_feature_list[item])
+    avg_bias = bias - (cal_support * avg_bias)
+
+    write_to_file(avg_bias, avg_feature_list)
+    print(avg_bias)
+
+main_func()
---
+++ per_classify.py
@@ -0,0 +1,57 @@
+import os, sys
+
+def reading():
+    s = open('per_model.txt', 'r').read()
+    return eval(s)
+
+def get_file_tokens(file):
+    feature_list = {}
+    file_open = open(file, "r", encoding="latin1")
+    file_content = file_open.read()
+
+    file_content_tokenized = file_content.split()
+    for word in file_content_tokenized:
+            feature_list[word] = 0
+    return feature_list
+
+def calculate_alpha(feature_list, tokens, bias):
+    val = 0
+    for token in tokens:
+        if token in feature_list:
+            val += feature_list[token]
+    val += bias
+    return val
+
+def write_to_file(target, line):
+    target.write(str(line))
+
+def main_func():
+        directory = sys.argv
+        model_content = reading()
+        output_file_name = directory[2]
+        bias = model_content["bias"]
+        feature_list = model_content["model"]
+        target = open(output_file_name, 'a')
+
+
+        for root, dirs, files in os.walk(directory[1]):
+            for file in files:
+                if file.endswith(".txt"):
+                    tokens = get_file_tokens(os.path.join(root, file))
+                    alpha = calculate_alpha(feature_list, tokens, bias)
+
+                    if alpha > 0:
+                        write_to_file(target, "SPAM " + os.path.join(root, file) + "\n")
+                    else:
+                        write_to_file(target, "HAM " + os.path.join(root, file) + "\n")
+                    # if "spam" in file:
+                    #     if alpha > 0:
+                    #         print(alpha,"spam","spam",file)
+                    #     else:
+                    #         print(alpha,"spam","ham",file)
+                    # else:
+                    #     if alpha > 0:
+                    #         print(alpha,"ham","spam",file)
+                    #     else:
+                    #         print(alpha,"ham","ham",file)
+main_func()
